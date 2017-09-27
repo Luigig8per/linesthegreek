cd7a4465b9a78a3ddb626060c0afeb4cc23ad444
Luigig8per <luisma@g8perhead.com>
2017-09-27 17:19:06 -0400

    Yes
    Ufff its working now//!!

--- linesthegreek.js
+++ linesthegreek.js
@@ -19,6 +19,10 @@ fs.readFile('body.html', 'utf8', function (err,data) {
 
 let frame1={
     "title": "h1", 
+
+"LEAGUES":
+{
+
     "Subtitle": "h3",
     
             "Sub":"h4",
@@ -33,22 +37,15 @@ let frame1={
 
             "title":"h3",
             "hour":".notes-bar",
-            "SpreadHome":".spread",
-            "MoneyLn":".money-line",
-            
-            "Total":".total",
            
-    
-            "Team total":".team-total",
-
             "Teams":{
                 "_s":"ul",
                 "_d":[{
-                    "Title": ".name",
+                    "Team": ".name",
         
                     "title":"h3",
                     "hour":".notes-bar",
-                    "SpreadHome":".spread",
+                    "Spread":".spread",
                     "MoneyLn":".money-line",
                     
                     "Total":".total",
@@ -61,80 +58,16 @@ let frame1={
 
         }]
     }
+
 }
-     let frame = {
-        "title": "h1", // this is an inline selector
-
-
-        "Subtitle": "h3",
-
-        "Sub":"h4",
-
-        "Date" : "h2",
-
-        "MoneyLn":".money-line",
-        "SpreadHome":".spread",
-        "Total":".total",
-        "Title": "name",
-
-        "Team total":".team-total",
-       
-       
-
-        "lines": {  
-            "selector": ".lines",   
-
-          "id": "id",
-
-         
-            
-          "team": {                                    // defining "url" by an attribute with "attr" and "selector" in an object
-          "selector": ".header [itemprop=name]",      // is actually the same as the inline selector
-          "attr": "href"                              // the attribute name to retrieve
-        },
-          
-
-        }
-       
-    }
+}
     
-
-
+    
     console.log("SPORTBOOK: "  + $('body').scrape(frame1, { string: true } ))
 
 
 
-    var frametest = {
-        "companies": {           // setting the parent item as "companies"
-            "selector": ".item",    // defines the elements to search for
-            "data": [{              // "data": [{}] defines a list of items
-                "name": ".header [itemprop=name]",          // inline selector defining "name" so "company"."name"
-                "description": ".header [rel=description]", // inline selector defining "description" as "company"."description"
-                "url": {                                    // defining "url" by an attribute with "attr" and "selector" in an object
-                    "selector": ".header [itemprop=name]",      // is actually the same as the inline selector
-                    "attr": "href"                              // the attribute name to retrieve
-                },
-                "contact": {                                // set up a parent "contact" element as "company"."contact"
-                    "selector": ".contact",                 // defines the element to search for
-                    "data": {                               // defines the data which "contact" will contain
-                        "telephone": {                          // using "type" to use "telephone" parser to extract only the telephone
-                            "selector": "[itemprop=telephone]",     // simple selector for "telephone"                
-                            "type": "telephone"                     // using "telephone" plugin parser
-                        },
-                        "employee": {                           // setting a parent node "employee" as "company"."contact"."employee"
-                            "name": "[itemprop=employeeName]",          // inline selector defining "name"
-                            "jobTitle": "[itemprop=employeeJobTitle]",  // inline selector defining "jobtitle"
-                            "email": {                          // using "type" to use "email" parser to extract only the email
-                                "selector": "[itemprop=email]",     // simple selector for "email"
-                                "type": "email"                     // using "email" plugin parser
-                            }
-                        }
-                    }
-                }
-            }]
-        }
-    
-    };
+   
 
 
     
@@ -144,19 +77,5 @@ let frame1={
 });
 
 
-let $ = cheerio.load(`
-	<body>
-		<h1>I love jsonframe!</h1>
-		<span itemprop="email"> Email: gabin@datascraper.pro  </span>
-	<body>`)
 
-    let jsonframe = require('jsonframe-cheerio')
-    jsonframe($) // initializing the plugin
-    
-    let frame = {
-        "title": "h1", // this is an inline selector
-        "email": "span[itemprop=email] < email" // output an extracted email
-    }
-    
-    console.log( $('body').scrape(frame, { string: true } ))
  
